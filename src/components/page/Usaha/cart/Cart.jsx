9604c0f92f13e8ce173f672c8a72694aaecb7e10
aslanariq237 <aslanariq009@gmail.com>
2024-09-13 14:18:39 +0700

    cart.jsx

--- src/components/page/Usaha/cart/Cart.jsx
+++ src/components/page/Usaha/cart/Cart.jsx
@@ -15,7 +15,9 @@ const Cart = () => {
       }).catch(err => console.log(err.message))
   }
   
-  const total = 
+  const total = cart.reduce((total, cart) => {
+    return total + cart.id_product.harga
+  }, 0)
 
   useEffect(() => {
     getItems()
@@ -43,10 +45,10 @@ const Cart = () => {
                       </div>
                       <div className="details">
                         <div className="sub-title">
-                          <p>{li.id_product.harga}</p>
+                          <p>Rp. {li.id_product.harga}</p>
                         </div>
                         <div className="qty">
-                          <p className='text-center'>{li.qty}</p>
+                          <p className='text-center text-sm'>{li.qty}</p>
                         </div>
                       </div>
                     </div>
@@ -63,68 +65,17 @@ const Cart = () => {
                     </div>
                   </div>
                 </div>
-                <div className="submit">
-                  <button type="submit" className='w-full bg-gray-300 rounded-md mt-3'>Bayar</button>
+                <div className="submit py-2">
+                  <button type="submit" className='w-full flex justify-between px-3 py-1 bg-gray-300 rounded-lg mt-3'>
+                    <p className='font-semibold'>Bayar</p>
+                    <p>Rp. {total}</p>
+                  </button>
                 </div>
               </div>
             </div>
           </div>
       }
     </div>
-    // <div className='bg-transparent py-4'>
-    //   {
-    //     cart.length === 0
-    //       ?
-    //       <div className="null bg-transparent text-center">
-    //         <p>Keranjang Anda Kosong</p>
-    //       </div>
-    //       :
-    //       <div className="container">
-    //         <div className="">
-    //           <div className="payment lg:w-2/5 border-l-2 px-5 mt-10 lg:mt-0">
-    //           <div className="title text-center">
-    //             <h1 className='text-2xl font-bold my-3'>Bajigur Caffe</h1>
-    //           </div>
-    //             {
-    //               cart.map((i, id) => (
-    //                 <div className="bayar my-2 border-b-2 py-2" key={id}>
-    //                   <div className="flex justify-between items-center">
-    //                     <p>{i.id_product.nama}</p>
-    //                     <div className="details text-center">
-    //                       <p className='text-xs'>Rp. {i.id_product.harga}</p>
-    //                       <p className='text-xs text-gray-500'>{i.qty}</p>
-    //                     </div>
-    //                   </div>
-    //                 </div>
-    //               ))
-    //             }
-    //             <div className="pymnt">
-    //               <p className='text-xl font-bold'>Payment Choice</p>
-    //               <div className="opsi">
-    //                 <div className="border-2 p-4 flex justify-around items-center">
-    //                   <div className="input-1">
-    //                     <input type="radio" name="p" id="" />Qris
-    //                   </div>
-    //                   <div className="input-2">
-    //                     <input type="radio" name="p" id="" />Cash
-    //                   </div>
-    //                 </div>
-    //               </div>
-    //             </div>
-    //             <div className="harga flex justify-between">
-    //               <p>Total</p>
-    //               <p>Rp. 200.000</p>
-    //             </div>
-    //             <div className="sbmit">
-    //               <button className="bg-blue-500 mt-3 *: rounded-full h-10  w-72 flex justify-between items-center px-4">
-    //                 <p>Bayar</p>
-    //               </button>
-    //             </div>
-    //           </div>
-    //         </div>
-    //       </div>
-    //   }
-    // </div>
   );
 };
 
